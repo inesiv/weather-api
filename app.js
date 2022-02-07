195d4ede55ecc31f04ab66f2386b906ed0c95fcc
inesiv <92948086+inesiv@users.noreply.github.com>
2022-02-07 12:06:57 +0200

    Update and rename script.js to app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+// LS object
+const ls = new LS()
+// init city data
+const initCity = ls.getCity()
+// Weather object
+const weather = new Weather('initCity')
+
+//UI object
+const ui = new UI()
+
+// events
+const form = document.querySelector('#change-city')
+form.addEventListener('submit', changeWeather)
+
+// change weather
+function changeWeather(event) {
+    const city = document.querySelector('#city-name').value
+    weather.changeCity(city)
+    ls.setCity(city)
+    getWeather()
+    document.querySelector('#city-name').value = ''
+    event.preventDefault()
+}
+
+
+// get city weather
+function getWeather(){
+    weather.getWeather()
+    .then(data => {
+        console.log(data)
+        ui.drawWeather(data)
+    })
+    .catch(error => console.log(error))
+}
+
+
+getWeather()
